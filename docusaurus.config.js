7f3b8b5c19560342e93adc6b51770a5130ad3353
Stefan Ernst <ernst.stefan@icloud.com>
2021-01-22 16:53:26 +0100

    Fixes to light mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ module.exports = {
     projectName: 'storymapping', // Usually your repo name.
     themeConfig: {
         colorMode: {
-            defaultMode: 'dark',
+            defaultMode: 'light',
             disableSwitch: true
         },
         navbar: {
