1a4568fc1f07af02b147196355fce9396a0c38e1
Stefan Ernst <ernst.stefan@icloud.com>
2021-01-22 16:42:24 +0100

    Adding plausible

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,118 +1,127 @@
 module.exports = {
-  title: 'Story Mapping for Jira',
-  tagline: 'Focus on user value & prioritize the right work',
-  url: 'https://storymapping.app',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/FreeIcon.svg',
-  organizationName: 'Realigned Technologies Ltd', // Usually your GitHub org/user name.
-  projectName: 'storymapping', // Usually your repo name.
-  themeConfig: {
-    colorMode: {
-      defaultMode: 'dark',
-      disableSwitch: true
+    title: 'Story Mapping for Jira',
+    tagline: 'Focus on user value & prioritize the right work',
+    url: 'https://storymapping.app',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/FreeIcon.svg',
+    organizationName: 'Realigned Technologies Ltd', // Usually your GitHub org/user name.
+    projectName: 'storymapping', // Usually your repo name.
+    themeConfig: {
+        colorMode: {
+            defaultMode: 'dark',
+            disableSwitch: true
+        },
+        navbar: {
+            style: 'dark',
+            title: 'Story Mapping for Jira',
+            logo: {
+                alt: 'Story Mapping for Jira Logo',
+                src: 'img/FreeIcon.svg',
+            },
+            items: [
+                {
+                    to: 'docs/',
+                    activeBasePath: 'docs',
+                    label: 'Docs',
+                    position: 'left',
+                },
+                /*{to: 'blog', label: 'Blog', position: 'left'},*/
+            ],
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Other Apps',
+                    items: [
+                        {
+                            label: 'Journey Mapping for Jira',
+                            to: 'https://realigned.io/journeymapping',
+                        },
+                        {
+                            label: 'Read Confirmations for Confluence',
+                            to: 'https://realigned.io/readconfirmations',
+                        },
+                        {
+                            label: 'Personal Dashboards for Confluence',
+                            to: 'https://realigned.io/personaldashboards',
+                        },
+                        {
+                            label: 'Enhanced Sharing for Confluence',
+                            to: 'https://realigned.io/enhancedsharing',
+                        },
+                    ],
+                },
+                {
+                    title: 'Important Links',
+                    items: [
+                        {
+                            label: 'Support',
+                            href: 'https://realignedtechnologies.atlassian.net/servicedesk/customer/portals',
+                            target: '_blank'
+                        },
+                        {
+                            label: 'EULA',
+                            href: 'https://docz.realignedtechnologies.com/eula',
+                        },
+                        {
+                            label: 'Privacy',
+                            href: 'https://docz.realignedtechnologies.com/privacypolicy',
+                        },
+                    ],
+                },
+                {
+                    title: 'Consultancy',
+                    items: [
+                        {
+                            label: "Realigned Services",
+                            to: 'https://realigned.ch',
+                            target: '_blank'
+                        },
+                        {
+                            label: 'Realigned Learning',
+                            href: 'https://realigned.work',
+                            target: '_blank'
+                        },
+                    ],
+                },
+            ],
+            logo: {
+                alt: 'Realigned Logo',
+                src: '/static/img/realigned.svg',
+            },
+            copyright: `Copyright © ${new Date().getFullYear()} Realigned Technologies Ltd. `,
+        },
     },
-    navbar: {
-      style: 'dark',
-      title: 'Story Mapping for Jira',
-      logo: {
-        alt: 'Story Mapping for Jira Logo',
-        src: 'img/FreeIcon.svg',
-      },
-      items: [
-        {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-        },
-        /*{to: 'blog', label: 'Blog', position: 'left'},*/
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Other Apps',
-          items: [
+    presets: [
+        [
+            '@docusaurus/preset-classic',
             {
-              label: 'Journey Mapping for Jira',
-              to: 'https://realigned.io/journeymapping',
-            },
-            {
-              label: 'Read Confirmations for Confluence',
-              to: 'https://realigned.io/readconfirmations',
-            },
-            {
-              label: 'Personal Dashboards for Confluence',
-              to: 'https://realigned.io/personaldashboards',
-            },
-            {
-              label: 'Enhanced Sharing for Confluence',
-              to: 'https://realigned.io/enhancedsharing',
-            },
-          ],
-        },
-        {
-          title: 'Important Links',
-          items: [
-            {
-              label: 'Support',
-              href: 'https://realignedtechnologies.atlassian.net/servicedesk/customer/portals',
-              target: '_blank'
-            },
-            {
-              label: 'EULA',
-              href: 'https://docz.realignedtechnologies.com/eula',
-            },
-            {
-              label: 'Privacy',
-              href: 'https://docz.realignedtechnologies.com/privacypolicy',
-            },
-          ],
-        },
-        {
-          title: 'Consultancy',
-          items: [
-            {
-              label: "Realigned Services",
-              to: 'https://realigned.ch',
-              target: '_blank'
-            },
-            {
-              label: 'Realigned Learning',
-              href: 'https://realigned.work',
-              target: '_blank'
-            },
-          ],
-        },
-      ],
-      logo: {
-        alt: 'Realigned Logo',
-        src: '/static/img/realigned.svg',
-      },
-      copyright: `Copyright © ${new Date().getFullYear()} Realigned Technologies Ltd. `,
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
 
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
 
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            },
+        ],
     ],
-  ],
+    scripts: [
+        {
+            /** <script async defer data-domain="storymapping.app" src="https://plausible.io/js/plausible.js"></script> **/
+            src: "https://plausible.io/js/plausible.js",
+            "data-domain": "storymapping.app",
+            async: true,
+            defer: true
+        }
+    ]
 };
