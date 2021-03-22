1275ad4513c516d3166b2d4c32d61243ccddc06e
Stefan Ernst <ernst.stefan@icloud.com>
2021-03-22 17:16:59 +0100

    Added Whatsnew

---
+++ src/pages/whatsnew.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function WhatsNew() {
+
+    return (
+        <div style={{padding:10}}>
+            <small>March 2021</small>
+            <h3>Improved Drag & Drop Handling</h3>
+
+            <p>
+                Lorem Ipsum Dolorem
+            </p>
+        </div>
+    )
+}
