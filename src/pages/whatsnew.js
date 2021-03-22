441a5e59b2c83bed77408c072fc358ab28429d5f
Stefan Ernst <ernst.stefan@icloud.com>
2021-03-22 17:33:47 +0100

    BG Color

--- src/pages/whatsnew.js
+++ src/pages/whatsnew.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import styles from './whatsnew.module.css';
 
 export default function WhatsNew() {
 
     return (
-        <div style={{padding:10}}>
+        <div style={{padding:10,backgroundColor:'white'}}>
             <small>March 2021</small>
             <h3>Improved Drag & Drop Handling</h3>
-
             <p>
                 Lorem Ipsum Dolorem
             </p>
