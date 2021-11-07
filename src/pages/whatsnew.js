7b7ced099d9766a34b9ddd8232b7f3a38c715632
Stefan Ernst <ernst.stefan@icloud.com>
2021-11-07 09:21:20 +0100

    Release Notes

--- src/pages/whatsnew.js
+++ src/pages/whatsnew.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './whatsnew.module.css';
 import Status from "../../docs/components/Status";
-import CustomCard from '../../static/img/CustomFields.png';
+import Stats from '../../static/img/stats.png';
 import Admin from '../../static/img/admin.png';
 
 
@@ -9,6 +9,38 @@ export default function WhatsNew({background='white'}) {
 
     return (
         <div style={{padding:10,backgroundColor:background}}>
+            <small>November 2021</small>
+            <div style={{display:'flex',gap:8, alignItems:'center',marginBottom:10}}>
+                <h3 style={{marginBottom:1}}>Pro Version Improvements 11/21</h3>
+                <span><Status text="Pro"  /> </span></div>
+            <p>
+                <ul>
+                    <li>Our most requested feature is here: You can now manage multiple user story maps.
+                        When you open the app forth the first time, you will be asked to select the map you want to work on. You can create
+                        many more user story maps with different boards and settings within the same project.
+                        You can also select a story map as a favorite and it will load automatically the next time you open the app.
+                    </li>
+                    <li>Introducing epic statistics: You can now see the progress of your issues within an epic. Toggle between
+                        totals per status, percentage of issues per status completed or percentage of story points done.
+                        <br />
+
+                        <img src={Stats} style={{width: 226,marginTop:10}} />
+                    </li>
+                    <li>Sub-task support: You can now display the number of subtasks on the issue card, alongside a
+                    link that reveals all subtasks and their status</li>
+                    <li>Quick-Filters from your agile board will be available from the redesigned filter menu</li>
+                    <li>Improved issue link import: Issues that are imported into the map via custom issue link will
+                    now be filtered correctly.</li>
+                    <li>Zoom in and out of your user story map with the new buttons in the bottom right hand corner</li>
+                    <li>Import steps automatically based on issue type when creating a user story map</li>
+                    <li>
+                        Better export: We have adjusted the card layout to improve export image quality.
+                    </li>
+                </ul>
+
+
+
+            </p>
             <small>September 2021</small>
             <div style={{display:'flex',gap:8, alignItems:'center',marginBottom:10}}>
                 <h3 style={{marginBottom:1}}>Pro Version Improvements 09/21 (DC / Server V1.7)</h3>
@@ -95,28 +127,7 @@ export default function WhatsNew({background='white'}) {
                     <li>Small visual improvements</li>
                 </ul>
             </p>
-            <small>May 2021</small>
-            <div style={{display:'flex',gap:8, alignItems:'center',marginBottom:10}}>
-                <h3 style={{marginBottom:1}}>Improvement Release 05/21</h3>
-                <span><Status text="Pro" /> </span></div>
-            <p>
-                <ul>
-                    <li>Improved create functionality for both epics and issues</li>
-                    <li>Improved scrolling behaviour on Server & DC</li>
-                    <li>Epic colors can now be used for the entire column</li>
-                    <li>Fixed a bug that prevented issue linking when a custom link type is used</li>
-                </ul>
-            </p>
-            <div style={{display:'flex',gap:8, alignItems:'center',marginBottom:10}}>
-                <h3 style={{marginBottom:1}}>Free Version Improvements 05/21</h3>
-                <span><Status text="Free" isFree /> </span></div>
-            <p>
-                <ul>
-                    <li>Improved create functionality for both epics and issues</li>
-                    <li>Minimize release, sprint or custom swimlanes</li>
-                    <li>Fixed display issues with long issue summaries</li>
-                </ul>
-            </p>
+
 
 
 
